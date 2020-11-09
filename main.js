093cc05233329233cd5fab95b7d4a07353015db2
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 10:11:47 +0300

    Loops-2

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+
+for (var count=0;count<7;count++){
+document.write('Current count:'+ count);
+document.write("<br/>");
+} 
+
+var mars;
+while(mars<4){document.write('This is meant to be:'+mars+"<br>");
+    mars++;
+}
